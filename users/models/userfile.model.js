878a347e48d56defc236e9896efcea9d1a6bf9ca
jimenezVazquez88 <danieljimenezvazquez@formacioonline.net>
2023-02-17 23:21:35 +0100

    hice un copia de un examen que hice esta tarde

--- users/models/userfile.model.js
+++ users/models/userfile.model.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 
 let assetsdir = path.join(__dirname,'../../../register-login-profile-mean-front-ex/src/assets/image');
-//let assetsdir = path.join(__dirname,'../../../rlp/src/assets/image')
+//let assetsdir = path.join(__dirname,'../../../rlp/src/assets/image123456')
 
 
 exports.createDirectory = async(user)=>{
